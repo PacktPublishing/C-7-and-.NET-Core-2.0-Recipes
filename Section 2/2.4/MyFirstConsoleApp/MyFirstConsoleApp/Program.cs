dedc26f15c1ac9c4e6e5ffce899f80fcf04d3bf3
Mrunal Dave <mrunaldave.packt@gmail.com>
2018-03-19 17:08:55 +0530

    code added

---
+++ Section 2/2.4/MyFirstConsoleApp/MyFirstConsoleApp/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace MyFirstConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What's your name?");
+            string name = Console.ReadLine();
+            Console.WriteLine("Hello, " + name);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Section 2/2.4/MyFirstConsoleApp/MySecondConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MySecondConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
